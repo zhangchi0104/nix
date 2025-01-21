49842a2a9b7b5d4f12b0037a9a1fb5b1072a7442
Alex Zhang <zhangchi0104@live.com>
2025-01-21 22:26:00 +1100

    initialized

---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "Example nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =  {self, nix-darwin, ... }:
+  let
+    configuration = { pkgs, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages = 
+        let 
+          pythonWithPackages = pkgs.python3.withPackages(ps: [ps.pynvim ps.rich ps.pydantic]);
+        in 
+          [ pkgs.vim
+            pkgs.neovim
+            pkgs.zellij
+            pkgs.bun
+            pkgs.btop
+            pkgs.bat
+            pkgs.lazygit
+            pkgs.starship
+            pkgs.git
+            pkgs.nodejs_20
+            pkgs.yarn
+            pythonWithPackages
+          ];
+      
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 5;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+
+      # Homebrew Configuration
+      homebrew  = {
+        enable = true;
+        brews = [];
+        casks = [
+          "ghostty"
+          "cursor"
+          "android-platform-tools"
+        ];
+      };
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#zhangchis-MacBook-Pro
+    darwinConfigurations."zhangchis-MacBook-Pro" = nix-darwin.lib.darwinSystem {
+      modules = [ configuration ];
+    };
+  };
+}
