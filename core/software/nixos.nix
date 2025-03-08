94de8b9d50ededeb310582278cfb09cd8de1dc78
Alex Zhang <alex@otakuma.dev>
2025-03-08 12:34:41 +1100

    updated

---
+++ core/software/nixos.nix
@@ -0,0 +1,13 @@
+{ config, lib, pkgs, ... }:
+
+{ 
+  environment.systemPackages = with pkgs; [
+    ghostty
+    code-cursor
+    slack
+    postman
+    android-tools
+    gcc
+    gnome-tweaks
+  ];
+}
