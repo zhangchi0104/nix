fd61b6da6e331e56867ae1b08e7123b7557e2bb5
Alex Zhang <zhangchi0104@live.com>
2025-05-17 21:59:21 +0800

    fix: update

--- home/command-line/programs/lazygit.nix
+++ home/command-line/programs/lazygit.nix
@@ -4,6 +4,11 @@
   programs.lazygit = {
     enable = true;
     package = pkgs.lazygit;
+    settings = {
+      services = {
+        "github.com" = "github:github.com";
+      };
+    };
   };
 
   catppuccin.lazygit.enable = true;
