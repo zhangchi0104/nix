a2aa3e2cd35863731d48fd8a2a75f7fe1e2f655d
Alex Zhang <zhangchi0104@live.com>
2025-06-06 16:49:54 +1000

    fix: update

--- home/command-line/zsh.nix
+++ home/command-line/zsh.nix
@@ -1,10 +1,32 @@
-{pkgs, config, ...} :
+{pkgs, lib, config, ...} :
+
 let
   androidHome = if isDarwin then "$HOME/Library/Android/sdk" else "$HOME/Android/sdk";
   isDarwin = pkgs.stdenv.isDarwin;
   osswitch-cmd = if isDarwin 
-    then "darwin-rebuild switch --flake ~/.config/nix" 
+    then "sudo darwin-rebuild switch --flake ~/.config/nix" 
     else "sudo nixos-rebuild switch --flake ~/.config/nix";
+  initExtraFirst=lib.mkOrder 500 ''
+      if [ $__ZSH_DEBUG ]; then
+        echo $PATH
+        echo $(which mv)
+        echo $(which dirname)
+        echo $(which mkdir)
+      fi
+      include() {
+        if [ -f $1 ]; then
+          source $1
+        fi
+      }
+      autoload -U edit-command-line
+      zle -N edit-command-line
+      bindkey -M vicmd v edit-command-line
+    '';
+  initExtra = lib.mkOrder 1000 ''
+      include ~/.zshrc.local
+      include ~/.config/zshrc.local
+    '';
+
 in
 {
   programs.zsh = {
@@ -35,25 +57,6 @@ in
     sessionVariables = {
       ANDROID_HOME = androidHome;
     };
-    initExtraFirst=''
-      if [ $__ZSH_DEBUG ]; then
-        echo $PATH
-        echo $(which mv)
-        echo $(which dirname)
-        echo $(which mkdir)
-      fi
-      include() {
-        if [ -f $1 ]; then
-          source $1
-        fi
-      }
-      autoload -U edit-command-line
-      zle -N edit-command-line
-      bindkey -M vicmd v edit-command-line
-    '';
-    initExtra = ''
-      include ~/.zshrc.local
-      include ~/.config/zshrc.local
-    '';
+    initContent = lib.mkMerge [ initExtraFirst initExtra ];
   };
 }
